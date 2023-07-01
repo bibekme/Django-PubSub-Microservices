ab443e295ab6c82b398f76162b3d74ef87dd466f
Bibek Khatri <bibekkhatri291@gmail.com>
2023-07-01 16:50:07 +0545

    Change db settings to postgres

--- consumer/core/settings.py
+++ consumer/core/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = "core.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "consumer_app",
+        "USER": "postgres",
+        "PASSWORD": "password",
+        "HOST": "db-2",
+        "PORT": "5433"
     }
 }
 
--- producer/core/settings.py
+++ producer/core/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = "core.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "producer_app",
+        "USER": "postgres",
+        "PASSWORD": "password",
+        "HOST": "db-1",
+        "PORT": "5432"
     }
 }
 
